e4e499b2f29b784b5abcef831d2e765b114da337
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-08-12 11:11:09 -0700

    MVP.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// contexts
+import {ProductContext} from './contexts/ProductContext.js';
+import {CartContext} from './contexts/CartContext.js';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -16,26 +20,39 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={{cart}}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+					{/* Routes */}
+					{/* old way to pass props */}
+					{/* <Route
+						exact
+						path="/"
+						render={() => (
+							<Products
+								products={products}
+								addItem={addItem}
+							/>
+						)}
+					/> */}
+					{/* new way to pass props */}
+					<Route 
+						exact 
+						path="/" 
+						component={Products} 
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</div>
+			</CartContext.Provider>
+
+		</ProductContext.Provider>
+		
 	);
 }
 
